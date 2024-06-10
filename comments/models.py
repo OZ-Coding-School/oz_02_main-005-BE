854535d30c9595b18cd835a7be0883ca3469c224
LEE-Hyuna1229 <yung00420@naver.com>
2024-06-10 18:52:29 +0900

    comments_model

---
+++ comments/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from members.models import Member
+from cardsets.models import Cardset
+
+# Create your models here.
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    cardset = models.ForeignKey(Cardset, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self',related_name='reply', on_delete=models.CASCADE, null=True, blank=True)
