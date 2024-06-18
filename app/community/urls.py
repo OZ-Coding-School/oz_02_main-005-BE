ca9713827cf65178b8454ae4ab6185fb0ef96300
seaj6382 <seaj6382@gmail.com>
2024-06-18 19:03:35 +0900

    community

--- app/community/urls.py
+++ app/community/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('community/rate_list/', RateListView.as_view(), name='rate_list'),
     path('community/new_list/', NewListView.as_view(), name='new_list'),
     path('community/save_list/', SaveListView.as_view(), name='save_list'),
-    path('community/cardset/<int:pk>/', CardsetDetailView.as_view(), name='cardset_public'),
     path('community/cardset/<int:pk>/rate/', cardset_rate, name='cardset_rate'),
     path('community/search/', cardset_search, name='cardset_search'),
     path('community/save/', cardset_save, name='cardset_save'),
